d866372bcd773029e6e5f9ba8f624f1953eee2c9
elkana-ltobing <elkana.ltobing@gmail.com>
2016-08-12 21:09:13 +0700

    menambah js

---
+++ js/math-basics.js
@@ -0,0 +1,29 @@
+// fungsi pangkat
+function pangkatAngka(x,y){
+var x = 2;
+var y = 3;
+var hasilPangkat = Math.pow(x,y);
+return hasilPangkat;
+}
+
+// fungsi akar kuadrat
+function akarKuadrat(z){
+var z = 9;
+var hasilKuadrat = Math.sqrt(z);
+return hasilKuadrat;
+}
+
+// fungsi hipotenusa
+function hipotenusa(a,b){
+var a = 3;
+var b = 4;
+var hasilHipotenusa = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+return hasilHipotenusa + " cm";
+}
+
+// fungsi luas lingkaran
+function luasLingkaran(r){
+var r = 7
+var luasLingkaran = Math.PI*(Math.pow(r,2));
+return Math.round(luasLingkaran)+" cm2";
+}
