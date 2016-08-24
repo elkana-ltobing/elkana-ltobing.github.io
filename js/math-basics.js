1cedd11502618da2ee6392146381db753c421fc0
name": null, & Hacktiv8 <pair+elkana-ltobing.hacktiv8@devbootcamp.com>
2016-08-24 11:21:07 +0700

    update terbaru

--- js/math-basics.js
+++ js/math-basics.js
@@ -1,29 +1,23 @@
 // fungsi pangkat
-function pangkatAngka(x,y){
-var x = 2;
-var y = 3;
-var hasilPangkat = Math.pow(x,y);
-return hasilPangkat;
+function pangkatAngka(x,y) {
+  return Math.pow(x,y);
 }
+pangkatAngka(2,3); // 8
 
 // fungsi akar kuadrat
 function akarKuadrat(z){
-var z = 9;
-var hasilKuadrat = Math.sqrt(z);
-return hasilKuadrat;
+  return Math.sqrt(z);
 }
+akarKuadrat(9); // 3
 
 // fungsi hipotenusa
 function hipotenusa(a,b){
-var a = 3;
-var b = 4;
-var hasilHipotenusa = Math.hypot(a, b);
-return hasilHipotenusa + " cm";
+return Math.hypot(a, b) + " cm";
 }
+hipotenusa(3,4); // 5cm
 
 // fungsi luas lingkaran
 function luasLingkaran(r){
-var r = 7
-var luasLingkaran = Math.PI*(Math.pow(r,2));
-return Math.round(luasLingkaran)+" cm2";
+return Math.round(Math.PI*(Math.pow(r,2)))+" cm2";
 }
+luasLingkaran(7); // 154 cm2
